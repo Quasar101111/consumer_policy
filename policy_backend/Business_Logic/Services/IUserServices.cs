697dcde6284ffb7e396c1a03fb1237ea65aa34b7
Rohan Kurian Mathew <Rohan.Kurian@nestgroup.net>
2025-06-30 10:05:03 +0530

    testing

---
+++ policy_backend/Business_Logic/Services/IUserServices.cs
@@ -0,0 +1,12 @@
+﻿using Data_Logic.Models;
+
+namespace Business_Logic.Services
+{
+    public interface IUserServices
+    {
+        Task<int> ChangePassword(string username, string oldPassword, string newPassword);
+        Task<(bool available, string message)> CheckUsernameAvailability(string username);
+        Task<(bool success, string? token, string? username, string? message)> Login(LoginDTO loginDTO);
+        Task<(bool success, string message)> Register(User user);
+    }
+}
---
+++ policy_backend/Data_Logic/Repository/IPolicyRepository.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Data_Logic.Repository
+{
+    public interface IPolicyRepository
+    {
+        Task<int> AddPolicy(string policyno, string userid);
+        Task<int> CheckChassisExists(string chassisno);
+        Task<int> CheckPolicyExists(string policyno);
+        Task<int> CheckPolicyVehicleExists(string policyno, string chassisno);
+        Task<bool> DeletePolicy(int id);
+        Task<object> GetBasicDetails(string policyno, string chassisno);
+        Task<string> GetUserId(string username);
+        Task<bool> ToggleStatus(int id);
+        Task<int> TotalPremium(string userid);
+        Task<List<string>> ViewPolicyNumber(string userid);
+    }
+}
